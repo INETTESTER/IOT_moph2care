8e3ce603746aa643638fe774d2c4bba1d7610891
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-02-21 14:24:28 +0700

    z

--- api/script.js
+++ api/script.js
@@ -3,8 +3,6 @@ import http from 'k6/http';
 export function ran() {
     const url = "https://www.ran-demon.com/Ranking";
     const res = http.get(url);
-    const url2 = "https://www.ran-demon.com/Ranking";
-    const res2 = http.get(url2);
 
     // วิธีที่ 2: ใช้ RegEx เพื่อดึง path จาก URL
     const path = url.replace(/^https?:\/\/[^\/]+/, ''); // ลบส่วนของ protocol (http:// หรือ https://)
