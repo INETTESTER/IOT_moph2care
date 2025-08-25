a83ad56e1a7ba4a8501f3ab15be9b13d0dec8501
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-08-25 13:10:22 +0700

    a

--- api/script.js
+++ api/script.js
@@ -4,10 +4,9 @@ export function ran() {
     const url = "https://www.ran-demon.com/Ranking";
     const res = http.get(url);
 
-    // วิธีที่ 2: ใช้ RegEx เพื่อดึง path จาก URL
-    const path = url.replace(/^https?:\/\/[^\/]+/, ''); // ลบส่วนของ protocol (http:// หรือ https://)
 
-    //console.log('Path: ' + path); // แสดง path ที่ดึงออกมา
+
+    //console.log(res.body); // แสดง path ที่ดึงออกมา
 
     return res
 }
